61aca23aa185bf2ec1488c7aad8dea6d4a9011ac
viktoriapython <78961157+viktoriapython@users.noreply.github.com>
2021-07-06 14:46:10 +0300

    Tests

--- Test_4.py
+++ Test_4.py
@@ -1,5 +1,8 @@
 user_word = input('Please write some words').split(',')
-user_word = list(user_word[::-1])
+#user_word = user_word[::-1]
 
-user_word.sort(reverse=True)
+#user_word.sort(ron)
 print(user_word)
+
+for num in user_word[::-1]:
+    print(num)
--- Test_5.py
+++ Test_5.py
@@ -1,7 +1,18 @@
 kort_a = (1, 2, 3, 5, 8)
 kort_b = (8, 2, 5)
 
+#kort_a = list(kort_a)
+#kort_b = list(kort_b)
+#kort_a.extend(kort_b)
+#rint(kort_a)
+
+
+
+kort_a = (1, 2, 3, 5, 8)
+kort_b = (8, 2, 5)
+
 kort_a = list(kort_a)
-kort_b = list(kort_b)
-kort_a.extend(kort_b)
+for x in kort_b:
+    kort_a.insert(2, x)
+kort_a = tuple(kort_a)
 print(kort_a)
